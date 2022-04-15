5ea8726ceb8a6c880c05997c6cee38a2ed8025b7
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-15 15:19:07 +0500

    7, - ,Import views inside the app URLs

--- django-project/djangoAppp/calc/urls.py
+++ django-project/djangoAppp/calc/urls.py
@@ -1,2 +1,5 @@
-#1. Import path into your created URL
+# Import path into your created URL
 from django.urls import path 
+
+# Import views inside the app URLs 
+from .import views 
--- test1.py
+++ test1.py
@@ -1,1 +1,1 @@
-579 print('This is a Django/Python file for testing pupose')
+587 print('This is a Django/Python file for testing pupose')
