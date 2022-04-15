8175a2d78dc7550ea9144e64a4c999cb6e8ff67c
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-15 13:39:08 +0500

    , - ,The file test1.py has been committed

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import time
 for i in range (1,5000):
     f = time.sleep(10)    # pauses for 5 seconds
     o = open("test1.py", "w")
-    o.write("{} print('This is a Python file for testing pupose')".format(i))
+    o.write("{} print('This is a Django/Python file for testing pupose')".format(i))
 
 
     os.system(r"C:\Users\test\Desktop\test\test\m.bat")
