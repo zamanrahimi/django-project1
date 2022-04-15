2e6454a4c1418bbe973e63e3a09df59554f9fc1b
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-15 15:48:20 +0500

    11, - ,Create templates folder

--- django-project/djangoAppp/calc/urls.py
+++ django-project/djangoAppp/calc/urls.py
@@ -8,6 +8,6 @@ from .import views
 
 urlpatterns = [
 	
-	path('', views.home, name='home')
+	path('', views.home)
 
 ]
--- test1.py
+++ test1.py
@@ -1,1 +1,1 @@
-655 print('This is a Django/Python file for testing pupose')
+760 print('This is a Django/Python file for testing pupose')
